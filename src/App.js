67db4c1ffb1c6b56b5a2311b1ef2bc8f958820a5
JHobson316 <hobsoncjordan@gmail.com>
2024-04-16 13:26:21 -0500

    Initial Push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+    const [serverResponse, setServerResponse] = useState('')
+    const url = process.env.REACT_APP_SERVER_URL;//'https://hackathon00-api.onrender.com/';
+
+    function callGet() {
+        axios.get(url)
+        .then((response) => {
+            setServerResponse(response.data);
+        });
+    }
+
+    function callPost() {
+        //http://localhost:8080/?lang=c++
+        let data = '?lang=nevara';
+        axios.post(url + data)
+        .then((response)=>{
+            setServerResponse(response.data);
+        });
+    }
+
+    function callPut() {
+        let data = '?mem=laezel&index=0';
+        axios.put(url + data)
+        .then((response) => {
+            setServerResponse(response.data);
+        })
+    }
+    function callDelete() {
+        let data = '?index=1';
+        axios.delete(url + data)
+        .then((response) => {
+            setServerResponse(response.data);
+        });
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {serverResponse}
+      <button onClick={callGet}>GET</button>
+      <button onClick={callPost}>POST</button>
+      <button onClick={callPut}>PUT</button>
+      <button onClick={callDelete}>DELETE</button>
     </div>
   );
 }
